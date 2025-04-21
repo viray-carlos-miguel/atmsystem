0fa4dec97323b7d6537d65f488f62c1f7b24ee65
viray-carlos-miguel <carlosviray2004@gmail.com>
2025-04-21 05:58:22 -0700

    testing ci/cd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.dev.cashNgo"
+    namespace = "com.dev.cashngo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.dev.cashNgo"
+        applicationId = "com.dev.cashngo"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- android/app/src/main/java/com/example/atmapp/MainActivity.java
+++ android/app/src/main/java/com/example/atmapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.atmapp;
+package com.dev.cashngo;
 
 import io.flutter.embedding.android.FlutterActivity;
 
